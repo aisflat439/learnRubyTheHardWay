409854b8c070d378c579fc1de01bc336749a7b0a
Devin <fitzsimonsdevin@gmail.com>
2016-10-20 15:46:36 -0400

    Ex 9 comlpete

--- ex9.rb
+++ ex9.rb
@@ -1,1 +1,12 @@
-ex9
+days = "Monday Tuesday Wendesday Thursday Friday Saturday Sunday"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type all we like.
+Even 4 lines if we like.
+"""
