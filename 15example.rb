cf573d3eeef19607358c6af2bb47084be1790727
aisflat439 <fitzsimonsdevin@gmail.com>
2016-10-22 11:04:48 -0400

    Added comments for the ruby commands

--- 15example.rb
+++ 15example.rb
@@ -1,13 +1,23 @@
+# grabs the first argument passed in to the script and puts it into filename
 filename = ARGV.first
 
+# uses the open method on the file stores that in txt
 txt = open(filename)
 
+#standard puts
 puts "Here is your file #{filename}"
+
+# txt is the stored variable from earlier, read puts that out.
 print txt.read
 
-print "Type the filename again: "
+#standard puts
+print "\n\nType the filename again: "
+
+#here we get teh value from the user instead
 file_again = $stdin.gets.chomp
 
+# call the open method which puts the value of the block in txt_again
 txt_again = open(file_again)
 
+#prints outs the block txt_again with the read method.
 print txt_again.read
