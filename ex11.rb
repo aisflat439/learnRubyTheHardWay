cb5eae10a81535f9f7d9c741fd9e5444184b3bd0
Devin <fitzsimonsdevin@gmail.com>
2016-10-22 09:19:22 -0400

    Completed ex 11, added a print andchomp flashcard to the deck.

--- ex11.rb
+++ ex11.rb
@@ -1,5 +1,8 @@
-print "How old are you?"
+print "How old are you? "
 age = gets.chomp
-print "How tall are you"
+print "How tall are you? "
 height = gets.chomp
-print "How much to you weigh?"
+print "How much to you weigh? "
+weight = gets.chomp
+
+puts "So you are #{age} years old, weight #{weight}, and are #{height} tall."
