fd2a7e99c97ef7a0420cf26abb92341d4ec4aa02
Devin <fitzsimonsdevin@gmail.com>
2016-10-22 10:11:41 -0400

    dialed in the to float example

--- 12example.rb
+++ 12example.rb
@@ -1,5 +1,19 @@
-print "Give me a number? "
+print "Give me a number: "
 number = gets.chomp.to_i
 
 bigger = number * 100
 puts "a bigger number is #{bigger}"
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "a smaller number is #{smaller}"
+
+puts "This shows how to take 10% off"
+print "Give me a number and I'll reduce it by 10%: "
+number = gets.chomp.to_f
+
+ten_percent = number - (number / 10)
+puts "Your number reduced by 10% is \v#{ten_percent}"
